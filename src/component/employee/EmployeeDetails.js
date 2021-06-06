ae12a87f0804711447ea52f83802a4e2a4fbee2f
pavansai54 <pavansai9440@gmail.com>
2021-06-06 11:35:29 +0530

    updated-API

--- src/component/employee/EmployeeDetails.js
+++ src/component/employee/EmployeeDetails.js
@@ -4,7 +4,6 @@ import { Link, useParams } from 'react-router-dom'
 import EmployeeService from '../services/EmployeeService'
 import { faBackward, faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 const Navbar = Styled.nav`
 background-color: ${props => props.bgColor};
 position: sticky;
@@ -63,129 +62,133 @@ const TableRow = Styled.tr`
 `
 const TableColumn = Styled.td`
 `
-const LinkTag = Styled(Link)` 
-color:black; 
+const LinkTag = Styled(Link)`
+color:black;
 text-decoration:none;
 `
-
 export const EmployeeDetails = () => {
-	const [error, setError] = useState(null)
-	const [isLoaded, setIsLoaded] = useState(false)
-	const { id } = useParams()
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const { id } = useParams()
+    const [empState, setState] = useState({
+        
+		code:'',
+        firstName: '',
+        lastName: '',
+        designation: ' ',
+        department: '',
+        email: '',
+        contactNo: '',
+        joiningDate: '',
+    })
+    useEffect(() => {
+        EmployeeService.getOneEmployeeDetail(id).then(
+            result => {
+				console.log(result)
+                setIsLoaded(true)
+                setState(result.data)
+            },
+            error => {
+                setIsLoaded(true)
+                setError(error)
+            }
+        )
+    }, [])
 
-	const [empState, setState] = useState({
-		name: '',
-		code: '',
-		email: ' ',
-		mobile: '',
-		department: '',
-		joinedDate: '',
-		role: '',
-	})
-
-	useEffect(() => {
-		EmployeeService.getOneEmployeeDetail(id).then(
-			result => {
-				setIsLoaded(true)
-				setState(result.data)
-			},
-			error => {
-				setIsLoaded(true)
-				setError(error)
-			}
-		)
-	}, [])
-
-	if (error) {
-		return <div>Error: {error.message}</div>
-	} else if (!isLoaded) {
-		return <div>Loading...</div>
-	}
-
-	return (
-		<Fragment>
-			<Navbar bgColor='grey' color='white'>
-				Employee Details
-				<Button>
-					<LinkTag to={'/list'}>
-						<FontAwesomeIcon icon={faBackward}></FontAwesomeIcon>
-					</LinkTag>
-				</Button>
-			</Navbar>
-			<Break />
-			<Container>
-				<Table>
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+        return <div>Loading...</div>
+    }
+    return (
+        <Fragment>
+            <Navbar bgColor='grey' color='white'>
+                Employee Details
+                <Button>
+                    <LinkTag to={'/list'}>
+                        <FontAwesomeIcon icon={faBackward}></FontAwesomeIcon>
+                    </LinkTag>
+                </Button>
+            </Navbar>
+            <Break />
+            <Container>
+                <Table>
 					<TableRow>
-						<TableColumn>
-							<Lable htmlFor='Name'> Name</Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.name} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							<Lable htmlFor='Empl-Id'> Employee Code </Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.code} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							<Lable htmlFor='Email'> Email </Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.email} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							{' '}
-							<Lable htmlFor='Mobile Number'> Mobile Number </Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.mobile} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							{' '}
-							<Lable htmlFor='Department'> Department</Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.department} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							<Lable htmlFor='Role'> Role</Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.role} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-					<TableRow>
-						<TableColumn>
-							{' '}
-							<Lable htmlFor='Date-Containerat' className='Selectbox1'>
-								{' '}
-								Join Date
-							</Lable>
-						</TableColumn>
-						<TableColumn>
-							:<Input value={empState.joinedDate} readOnly />
-						</TableColumn>
-					</TableRow>
-					<Break />
-				</Table>
-			</Container>
-		</Fragment>
-	)
+                        <TableColumn>
+                            <Lable htmlFor='code'> Code</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.code} />
+                        </TableColumn>
+                    </TableRow>
+					<Break/>
+                    <TableRow>
+                        <TableColumn>
+                            <Lable htmlFor='firstName'> FirstName</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.firstName} />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            <Lable htmlFor='lastName'> LastName</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.lastName} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            <Lable htmlFor='designation'> Designation</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.designation} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            <Lable htmlFor='department'>Department </Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.department} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            {' '}
+                            <Lable htmlFor='email'>Email </Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.email} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            {' '}
+                            <Lable htmlFor='contactNo'> ContactNo</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.contactNo} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                    <TableRow>
+                        <TableColumn>
+                            <Lable htmlFor='joiningDate'> JoiningDate</Lable>
+                        </TableColumn>
+                        <TableColumn>
+                            :<Input value={empState.joiningDate} readOnly />
+                        </TableColumn>
+                    </TableRow>
+                    <Break />
+                </Table>
+            </Container>
+        </Fragment>
+    )
 }
